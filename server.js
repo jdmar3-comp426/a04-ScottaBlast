e430b22606d0b01c9695d91860f0817509906515
ScottaBlast <you@example.com>
2021-11-02 19:06:08 -0400

    md5 try

--- server.js
+++ server.js
@@ -46,8 +46,10 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id = ?")
-	.run(req.body.user, req.body.pass, req.params.id);
-	res.status(200).json(stmt)
+	.run(req.body.user, md5(req.body.pass), req.params.id);
+	res.status(200);
+	res.json({"message":stmt.changes + " record updated: ID " + req.params.id + " (200)"});
+	res.json(stmt);
 	
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
