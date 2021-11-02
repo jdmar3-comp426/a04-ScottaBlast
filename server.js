c95df0ca86fd0ee4d0a4a09b15b338ba2767c8a5
ScottaBlast <you@example.com>
2021-11-02 18:42:57 -0400

    update fix 4

--- server.js
+++ server.js
@@ -45,8 +45,10 @@ app.get("/app/user/:id", (req, res) => {
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user:id", (req, res) => {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id = ?").run(any, any, req.params.id);
-	res.status(200).json(stmt);
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id = ?").run(req.body.user, req.body.user, req.params.id);
+	res.status(200);
+	res.json({"message":stmt.changes + " record updated: ID " + stmt.lastInsertRowid + " (200)"});
+	res.json(stmt);
 	
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
